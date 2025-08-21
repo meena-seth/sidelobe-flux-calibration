c9ed89232aa2858197553ad7c1b318c6629b8dd2
meena-seth <mseth2@uw.edu>
2025-08-21 15:46:09 +0000

    Remove copies of holography scripts

--- holo_script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --account=def-halpern
-#SBATCH --ntasks=1               # number of MPI processes
-#SBATCH --mem=8GB      # memory; default unit is megabytes
-#SBATCH --time=0-01:00           # time (DD-HH:MM)
-module use /project/rpp-chime/chime/chime_env/modules/modulefiles/
-module load chime/python/2025.03
-source ~/meenaseth/bin/activate
-python /home/mseth2/scratch/nrao/trying_on_cedar/holo_script.py $1
--- submit_holo_script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-for i in $(seq 0 1023);
-do
-    sbatch /home/mseth2/scratch/nrao/trying_on_cedar/holo_script.sh $i
-done
-
