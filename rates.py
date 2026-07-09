a590d155f95c64279f9ec928289d4a5ad159ab7c
Meena Seth <mseth2@uw.edu>
2026-07-08 17:39:28 -0700

    Rate calculations for HA >= 60deg

---
+++ rates.py
@@ -0,0 +1,47 @@
+import numpy as np
+import astropy.units as u
+from astropy.time import Time 
+from astroplan import Observer, FixedTarget, is_observable
+from astroplan.plots import plot_finder_image
+from astroplan.constraints import Constraint, AltitudeConstraint
+import matplotlib.pyplot as plt
+import pdb
+
+ha_lim = 60
+dt = 5 * u.minute
+start_mjd = 58406
+end_mjd = 60776
+
+chime = Observer.at_site('chime')
+crab = FixedTarget.from_name('Crab Pulsar')
+
+time_range = Time([start_mjd, end_mjd], format='mjd', scale='utc')
+alt_constraint = AltitudeConstraint(min=0 * u.deg) #Has to be in the sky 
+alt_mask = is_observable(
+    constraints=[alt_constraint],
+    observer=chime,
+    targets=[crab],
+    time_range=time_range,
+    time_grid_resolution=dt,
+)[0]
+
+n_steps = int(((time_range[1] - time_range[0]) / dt).decompose().value)
+times = time_range[0] + np.arange(n_steps) * dt #Making array of all the times 
+ha = chime.target_hour_angle(times, crab) #Calculate HA for each
+ha_deg = ha.wrap_at(180 * u.deg).deg
+
+ha_mask_e = (ha_deg >= -180) & (ha_deg <= -ha_lim)
+ha_mask_w = (ha_deg >= ha_lim) & (ha_deg <= 180)
+
+e_steps = alt_mask & ha_mask_e
+w_steps = alt_mask & ha_mask_w
+total_steps = sum(e_steps + w_steps) 
+
+total_obs_time = (time_range[1] - time_range[0]).to(u.hour)
+total_eff_time = (total_steps * dt).to(u.hour)
+
+print(f"""Between MJD {start_mjd} - {end_mjd}:
+            Crab was in the sky above CHIME for {total_obs_time:.2f}
+            Crab was outside +/- {ha_lim} for {total_eff_time:.2f}""")
+
+pdb.set_trace()
